4d59fda71978ece7875b167ff9ec872e515b8b04
서지수 <diduedidue@naver.com>
2025-01-21 21:57:23 +0900

    refactor: BottomNavigationBar 위젯 분리 (#1)
    - 공통으로 사용되는 BottomNavigationBar를 별도 위젯으로 분리함.

---
+++ fe/lib/widgets/bottom_navigation_bar.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import '../pages/home_page.dart';
+import '../pages/adjustment_page.dart';
+import '../pages/user_page.dart';
+
+class CustomBottomNavigationBar extends StatelessWidget {
+  final int currentIndex;
+
+  const CustomBottomNavigationBar({
+    super.key,
+    required this.currentIndex,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      backgroundColor: Colors.white,
+      currentIndex: currentIndex,
+      items: const [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home),
+          label: '홈',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.list),
+          label: '정산 목록',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.person),
+          label: '내 정보',
+        ),
+      ],
+      selectedItemColor: const Color(0xFF22BE67),
+      unselectedItemColor: Colors.grey,
+      showUnselectedLabels: true,
+      onTap: (index) {
+        switch (index) {
+          case 0:
+            Get.off(() => const HomePage());
+            break;
+          case 1:
+            Get.off(() => const AdjustmentPage());
+            break;
+          case 2:
+            Get.off(() => const UserPage());
+            break;
+        }
+      },
+    );
+  }
+}
